1530093c71360732b67b27d89d4f646e5a8d5963
Guillermo Crespo Aguayo <34092854+DSMan97@users.noreply.github.com>
2018-10-09 12:16:57 +0200

    Update videojuegos.sql

--- src/videojuegos.sql
+++ src/videojuegos.sql
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 --
 
 ----------------------------------------------------------
-CREATE DATABASE VIDEOJUEGOS;
+CREATE DATABASE videojuegos;
 --
 -- Estructura de tabla para la tabla `videojuegos`
 --
